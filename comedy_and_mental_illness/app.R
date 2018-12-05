5c50c50d89e4bc338d0a25daf60cff7f6c65bdf4
John Ball <johnball@Johns-MacBook-Pro-10.local>
2018-12-04 22:30:40 -0500

    fixed text formatting

--- comedy_and_mental_illness/app.R
+++ comedy_and_mental_illness/app.R
@@ -40,12 +40,6 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("age",
-                     "Number of bins:",
-                     min = 14,
-                     max = 90,
-                     value = 30),
-         
          selectInput("groups", "Comedian Demographics",
                      choices = c("Men", "Women", "Hobbyists", "Professionals", "Amateur Pursuing Professional Career", "Mixed Professional/Unprofessional Income"))
       ),
@@ -55,24 +49,29 @@ ui <- fluidPage(
         tabsetPanel(type = "tabs",
                     tabPanel("Background", textOutput("bg")),
                     
-                    tabPanel("Big 5 Personality Traits", textOutput("big5e"),
+                    tabPanel("Big 5 Personality Traits", 
+                             h5("Formatted as 'mean(sd)':"),
+                             textOutput("big5e"),
                              textOutput("big5a"), textOutput("big5c"),
                              textOutput("big5n"), textOutput("big5i"),
                              img(src = 'Big 5.png', align = "right")),
                     
                     tabPanel("Depression", plotOutput("distPlot"),      
-                             img(src = '1471-244X-1-3-2.jpg', align = "right"),
+                             img(src = '1471-244X-1-3-2.jpg', align = "center"),
                              plotOutput("joyPlot")),
                     
                     tabPanel("Hypomania", plotOutput("hPlot"),
                              textOutput("htable")),
                     
                     tabPanel("Alcoholism", 
+                             h5("Formatted as 'mean(sd)':"),
                              textOutput("cons"), textOutput("dep"),
-                             textOutput("probs"), text("AUDITtot")),
+                             textOutput("probs"), textOutput("AUDITtot")),
                     
-                    tabPanel("Psychosis", textOutput("IntAn"),
-                             textOutput("UnEx"), textOutput("CogDis"),
+                    tabPanel("Psychosis", 
+                             h5("Formatted as 'mean(sd)':"),
+                             textOutput("UnEx"),
+                             textOutput("CogDis"), textOutput("IntAn"), 
                              textOutput("ImpNon"), textOutput("OLIFEtot"),
                              img(src = 'O-LIFE.png', align = "right"))
         )
@@ -124,10 +123,8 @@ server <- function(input, output) {
               ")")
    })
    
-   #how to filter out NA values in the string?
-   
    output$big5e <- renderText({
-     paste0(round(mean(datasetInput()$Extraversion, na.rm = T), digits = 2),
+     paste0("Extraversion: ", round(mean(datasetInput()$Extraversion, na.rm = T), digits = 2),
              " (",
              round(sd(datasetInput()$Extraversion, na.rm = T), digits = 2),
              ")")
@@ -135,30 +132,91 @@ server <- function(input, output) {
    
    
     output$big5a <- renderText({  
-     paste0(round(mean(datasetInput()$Agreeableness, na.rm = T), digits = 2),
+     paste0("Agreeableness: ", round(mean(datasetInput()$Agreeableness, na.rm = T), digits = 2),
             " (",
             round(sd(datasetInput()$Agreeableness, na.rm = T), digits = 2),
             ")")     
    })
     
     output$big5c <- renderText({  
-      paste0(round(mean(datasetInput()$Conscientiousness, na.rm = T), digits = 2),
+      paste0("Conscientiousness: ", round(mean(datasetInput()$Conscientiousness, na.rm = T), digits = 2),
              " (",
              round(sd(datasetInput()$Conscientiousness, na.rm = T), digits = 2),
              ")")     
     })
     
     output$big5n <- renderText({  
-      paste0(round(mean(datasetInput()$Neuroticism, na.rm = T), digits = 2),
+      paste0("Neuroticism: ", round(mean(datasetInput()$Neuroticism, na.rm = T), digits = 2),
              " (",
              round(sd(datasetInput()$Neuroticism, na.rm = T), digits = 2),
              ")")     
     })
     
     output$big5i <- renderText({  
-      paste0(round(mean(datasetInput()$Imagination, na.rm = T), digits = 2),
+      paste0("Imagination: ", round(mean(datasetInput()$Imagination, na.rm = T), digits = 2),
              " (",
              round(sd(datasetInput()$Imagination, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+    #dep, probs, AUDITtot
+    output$cons <- renderText({  
+      paste0("Consumption: ", round(mean(datasetInput()$Consumption, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$Consumption, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+    output$dep <- renderText({  
+      paste0("Dependance: ", round(mean(datasetInput()$Dependance, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$Dependance, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+    output$probs <- renderText({  
+      paste0("Problems: ", round(mean(datasetInput()$Problems, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$Problems, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+    output$AUDITtot <- renderText({  
+      paste0("Total Score: ", round(mean(datasetInput()$AUDITtot, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$AUDITtot, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+  #IntAn, UnEx, CogDis, ImpNon, OLIFEtot
+   
+    output$UnEx <- renderText({  
+      paste0("Unusual Experiences: ", round(mean(datasetInput()$OLA, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$OLA, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+    
+    output$IntAn <- renderText({  
+      paste0("Introverted Anhedonia: ", round(mean(datasetInput()$IntAnh, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$IntAnh, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+    
+    output$CogDis <- renderText({  
+      paste0("Cognitive Distortions: ", round(mean(datasetInput()$OCogDis, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$OCogDis, na.rm = T), digits = 2),
+             ")")     
+    })
+    
+    output$ImpNon <- renderText({  
+      paste0("Impulsive Nonconformity: ", round(mean(datasetInput()$NonConf, na.rm = T), digits = 2),
+             " (",
+             round(sd(datasetInput()$NonConf, na.rm = T), digits = 2),
              ")")     
     })
 }
